74e4d86b1dc13a4681b07af425513de522be8ff2
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2023-04-16 22:16:45 -0500

    add human to js project english

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -9,6 +9,7 @@ const Chip = ({ tag }: ChipProps) => (
     style={{
       backgroundColor: tags[tag].backgroundColor,
       borderColor: tags[tag].borderColor,
+      color: tags[tag]?.textColor,
     }}
     className={`flex items-center gap-[2px] rounded-[20px] px-[6px] py-[2px] border-[1px]`}>
     <div className='grid place-items-center w-[20px] h-[20px] p-[1px] bg-white1 rounded-full'>
