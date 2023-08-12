12dbc3f8a5fc23eb73328d0194276b552895221e
Juan Carlos Manzanero Domínguez <juancmandev@protonmail.com>
2023-08-11 23:32:36 -0600

    add sidebar from shadcn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import { HamburgerIcon, CloseIcon } from '@/assets/Icons';
+import { HamburgerIcon } from '@/assets/Icons';
 import Tippy from '@tippyjs/react';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+  SheetClose,
+} from '@/components/ui/sheet';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import 'tippy.js/dist/tippy.css';
 
 const dictionary: any = {
@@ -44,156 +56,117 @@ const navItems = [
   },
 ];
 
-const Header = ({ lang }: any) => {
+export default function Header({ lang }: any) {
   const changeLang = lang === 'en' ? 'es' : 'en';
-  const [windowSize, setWindowSize] = useState<number[]>([0, 0]);
-  const sidebar = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-    const handleWindowResize = () =>
-      setWindowSize([window.innerWidth, window.innerHeight]);
-
-    window.addEventListener('resize', handleWindowResize);
-
-    handleWindowResize();
-
-    return () => window.removeEventListener('resize', handleWindowResize);
-  }, []);
-
-  const handleDialogClick = (
-    e: React.MouseEvent<HTMLDialogElement, MouseEvent>
-  ) => {
-    if (e.currentTarget.tagName !== 'DIALOG') return;
-
-    const rect = e.currentTarget.getBoundingClientRect();
-
-    const clickedInDialog =
-      rect.top <= e.clientY &&
-      e.clientY <= rect.top + rect.height &&
-      rect.left <= e.clientX &&
-      e.clientX <= rect.left + rect.width;
-
-    if (clickedInDialog === false) e.currentTarget.close();
-  };
-
-  useEffect(() => {
-    const handleWindowResize = () =>
-      setWindowSize([window.innerWidth, window.innerHeight]);
-
-    window.addEventListener('resize', handleWindowResize);
-
-    handleWindowResize();
-
-    return () => window.removeEventListener('resize', handleWindowResize);
-  }, []);
 
   return (
-    <>
-      <header className='w-full sticky p-6 md:px-15 -top-1 z-10 flex justify-between items-center bg-dark1 shadow-md shadow-boxShadow'>
-        <nav className='w-full max-w-[1200px] xl:mx-auto flex justify-between items-center'>
-          <section>
-            <Link
-              className='outline-none focus:underline hover:underline text-xl text-primary focus:text-primaryLight hover:text-primaryLight'
-              href={`/${lang}`}>
-              juancmandev
-            </Link>
-          </section>
-          <section className='hidden sm:flex items-center gap-8'>
-            <Tippy
-              placement='left'
-              content={
-                lang === 'en'
-                  ? `Cambiar idioma al ${
-                      changeLang === 'en' ? 'Inglés' : 'Español'
-                    }`
-                  : `Change language to ${
-                      changeLang === 'en' ? 'English' : 'Spanish'
-                    }`
-              }>
+    <Sheet>
+      <TooltipProvider>
+        <header className='w-full sticky p-6 md:px-15 -top-1 z-10 flex justify-between items-center bg-dark1 shadow-md shadow-boxShadow'>
+          <nav className='w-full max-w-[1200px] xl:mx-auto flex justify-between items-center'>
+            <section>
               <Link
-                className='outline-none focus:underline hover:underline'
-                tabIndex={windowSize[0] < 728 ? -1 : 0}
-                href={`/${changeLang}`}>
-                <p className='text-lg'>{changeLang === 'en' ? '🇺🇸' : '🇲🇽'}</p>
+                className='outline-none focus:underline hover:underline text-xl text-primary focus:text-primaryLight hover:text-primaryLight'
+                href={`/${lang}`}>
+                juancmandev
               </Link>
-            </Tippy>
-            <ul className='flex items-center gap-8'>
-              {navItems.map((navItem) => (
-                <li key={navItem.label} className='w-max h-max'>
+            </section>
+            <section className='hidden sm:flex items-center gap-8'>
+              <Tooltip>
+                <TooltipTrigger asChild>
                   <Link
-                    tabIndex={windowSize[0] < 728 ? -1 : 0}
-                    href={`/${lang}${navItem.to}`}
-                    className='outline-none font-bold hover:underline focus:underline text-lg'>
-                    {dictionary[navItem.label][lang]}
+                    className='px-2 focus:underline hover:underline'
+                    href={`/${changeLang}`}>
+                    <p className='text-lg'>
+                      {changeLang === 'en' ? '🇺🇸' : '🇲🇽'}
+                    </p>
                   </Link>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </nav>
+                </TooltipTrigger>
+                <TooltipContent
+                  side='bottom'
+                  className='px-2 py-1 bg-dark2 border-0'>
+                  <p>
+                    {lang === 'en'
+                      ? `Cambiar idioma al ${
+                          changeLang === 'en' ? 'Inglés' : 'Español'
+                        }`
+                      : `Change language to ${
+                          changeLang === 'en' ? 'English' : 'Spanish'
+                        }`}
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+              <ul className='flex items-center gap-8'>
+                {navItems.map((navItem) => (
+                  <li key={navItem.label} className='w-max h-max'>
+                    <Link
+                      href={`/${lang}${navItem.to}`}
+                      className='outline-none font-bold hover:underline focus:underline text-lg'>
+                      {dictionary[navItem.label][lang]}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </nav>
 
-        <section className='flex sm:hidden h-max items-center'>
-          <Tippy
-            trigger='none'
-            placement='left'
-            content={lang === 'en' ? 'Open Side Menu' : 'Abrir Menú Lateral'}>
-            <button
-              className='outline-none focus:text-primary hover:text-primary'
-              onClick={() => sidebar.current?.showModal()}
-              id='toggle-menu'>
-              <HamburgerIcon size={32} />
-            </button>
-          </Tippy>
-        </section>
-      </header>
-
-      <dialog
-        onClick={handleDialogClick}
-        ref={sidebar}
-        className='px-0 py-8 min-h-screen ml-0 bg-dark1 text-white1 shadow-md shadow-boxShadow backdrop:bg-[rgba(5,_5,_5,_0.5)]'>
-        <div className='w-full flex justify-center'>
-          <button
-            className='p-2 outline-none flex justify-center focus:text-primary hover:text-primary w-full my-4 py-3 text-center focus:underline hover:bg-[rgba(0,_0,_0,_0.2)]  focus:bg-[rgba(0,_0,_0,_0.2)]'
-            onClick={() => sidebar.current?.close()}>
-            <CloseIcon size={32} />
-          </button>
-        </div>
-        <nav>
-          <section className='flex flex-col mt-10'>
+          <section className='flex sm:hidden h-max items-center'>
             <Tippy
+              trigger='none'
               placement='left'
-              content={
-                lang === 'en'
-                  ? `Cambiar idioma al ${
-                      changeLang === 'en' ? 'Inglés' : 'Español'
-                    }`
-                  : `Change language to ${
-                      changeLang === 'en' ? 'English' : 'Spanish'
-                    }`
-              }>
-              <Link
-                className='outline-none w-full my-4 py-3 text-center hover:underline focus:underline hover:bg-[rgba(0,_0,_0,_0.2)]  focus:bg-[rgba(0,_0,_0,_0.2)]'
-                href={`/${changeLang}`}>
-                <p className='text-2xl'>{changeLang === 'en' ? '🇺🇸' : '🇲🇽'}</p>
-              </Link>
+              content={lang === 'en' ? 'Open Side Menu' : 'Abrir Menú Lateral'}>
+              <SheetTrigger>
+                <HamburgerIcon size={28} />
+              </SheetTrigger>
             </Tippy>
-            <ul className='flex flex-col gap-2'>
-              {navItems.map((navItem) => (
-                <li key={navItem.label} className='w-full h-max flex'>
-                  <Link
-                    href={`/${lang}${navItem.to}`}
-                    onClick={() => sidebar.current?.close()}
-                    className='outline-none w-full px-10 py-3 font-bold text-center text-lg hover:underline focus:underline hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
-                    {dictionary[navItem.label][lang]}
-                  </Link>
-                </li>
-              ))}
-            </ul>
           </section>
-        </nav>
-      </dialog>
-    </>
+        </header>
+
+        <SheetContent
+          side='right'
+          className='w-max text-center px-0 py-15 border-0 shadow-boxShadow shadow-md bg-dark1'>
+          <SheetHeader>
+            <nav>
+              <section className='flex flex-col mt-10'>
+                <Tippy
+                  placement='left'
+                  content={
+                    lang === 'en'
+                      ? `Cambiar idioma al ${
+                          changeLang === 'en' ? 'Inglés' : 'Español'
+                        }`
+                      : `Change language to ${
+                          changeLang === 'en' ? 'English' : 'Spanish'
+                        }`
+                  }>
+                  <SheetClose asChild>
+                    <Link
+                      className='outline-none cursor-default w-full my-4 py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'
+                      href={`/${changeLang}`}>
+                      <p className='text-2xl'>
+                        {changeLang === 'en' ? '🇺🇸' : '🇲🇽'}
+                      </p>
+                    </Link>
+                  </SheetClose>
+                </Tippy>
+                <ul className='flex flex-col gap-2'>
+                  {navItems.map((navItem) => (
+                    <li key={navItem.label} className='w-full h-max flex'>
+                      <SheetClose asChild>
+                        <Link
+                          href={`/${lang}${navItem.to}`}
+                          className='outline-none cursor-default w-full px-10 py-3 font-bold text-center text-lg hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
+                          {dictionary[navItem.label][lang]}
+                        </Link>
+                      </SheetClose>
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            </nav>
+          </SheetHeader>
+        </SheetContent>
+      </TooltipProvider>
+    </Sheet>
   );
-};
-
-export default Header;
+}
