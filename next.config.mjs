9817e7d94e8e19f40b40b622d71ea5e5bfb13376
Juan Carlos Manzanero Domínguez <juancmandev@protonmail.com>
2023-09-19 18:59:53 -0600

    remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,17 @@ import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: 'dist',
   images: {
     domains: ['www.juancman.dev'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
   },
 };
 
