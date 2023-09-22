7a6b3b565a3862ab6fb140f962d3fbf320402f8e
Juan Carlos Manzanero Domínguez <59628111+juancmandev@users.noreply.github.com>
2023-09-22 16:59:12 +0000

    Remove JS Tooltips, replace with title property, update Footer (#50)
    * Change feed title and favicon (#46)
    
    * add random change
    
    * remove random change
    
    * static build
    
    * remove static export
    
    * add rss feed correctly
    
    * static build
    
    * change feed title and add favicon url
    
    * remove final slash
    
    * remove emojis
    
    * Change feed title and favicon (#46)
    
    * add random change
    
    * remove random change
    
    * static build
    
    * remove static export
    
    * add rss feed correctly
    
    * static build
    
    * change feed title and add favicon url
    
    * add final periot
    
    * add root page to redirect both site languages
    
    * remove ks tooltips, replace with title property, update Footer
    
    * remove random change

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import '@/styles/globals.css';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main className='px-5 py-15 flex justify-center items-center bg-dark1 text-white1 font-light min-h-screen'>
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-2xl text-center'>
+          This is my website, choose your language.
+        </h1>
+        <h1 className='text-2xl text-center'>
+          Este es mi website, escoge tu idioma.
+        </h1>
+        <div className='flex justify-center gap-10'>
+          <Link title='English' className='text-7xl' href='/en'>
+            🇺🇸
+          </Link>
+          <Link title='Español' className='text-7xl' href='/es'>
+            🇲🇽
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { EmailIcon, GitHubIcon, HamburgerIcon, RSSIcon } from '@/assets/Icons';
-import Tippy from '@tippyjs/react';
 import {
   Sheet,
   SheetContent,
@@ -10,13 +9,6 @@ import {
   SheetTrigger,
   SheetClose,
 } from '@/components/ui/sheet';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
-import 'tippy.js/dist/tippy.css';
 
 const dictionary: any = {
   ['home']: {
@@ -34,6 +26,15 @@ const dictionary: any = {
   ['contact']: {
     ['en']: 'Contact',
     ['es']: 'Contacto',
+  },
+  ['change_language']: {
+    ['en']: 'Cambiar idioma al Español',
+    ['es']: 'Change language to English',
+  },
+  ['change_language_flag']: { ['en']: '🇲🇽', ['es']: '🇺🇸' },
+  ['open_side_menu']: {
+    ['en']: 'Open Side Menu',
+    ['es']: 'Abrir Menú Lateral',
   },
 };
 
@@ -66,198 +67,136 @@ export default function Header({ lang }: any) {
 
   return (
     <Sheet>
-      <TooltipProvider>
-        <header className='w-full p-6 md:px-15 z-10 flex justify-between items-center bg-dark1 shadow-md shadow-boxShadow'>
-          <nav className='w-full max-w-[1200px] xl:mx-auto flex justify-between items-center'>
+      <header className='w-full p-6 md:px-15 sticky -top-1 z-10 flex justify-between items-center bg-dark1 shadow-md shadow-boxShadow'>
+        <nav className='w-full max-w-[1200px] xl:mx-auto flex justify-between items-center'>
+          <section>
+            <Link
+              className='outline-none focus:underline hover:underline text-xl text-primary focus:text-primaryLight hover:text-primaryLight'
+              href={`/${lang}`}>
+              juancmandev
+            </Link>
+          </section>
+          <section className='hidden sm:flex items-center gap-8'>
+            <ul className='flex items-center gap-8'>
+              {navItems.map((navItem) => (
+                <li key={navItem.label} className='w-max h-max'>
+                  <Link
+                    href={`/${lang}${navItem.to}`}
+                    className='outline-none font-bold hover:underline focus:underline text-lg'>
+                    {dictionary[navItem.label][lang]}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className='hidden sm:block'>
+            <ul className='flex items-center gap-6'>
+              <li className='flex'>
+                <Link
+                  title={dictionary['change_language'][lang]}
+                  className='px-2 text-xl focus:underline hover:underline'
+                  href={`/${changeLang}`}>
+                  {dictionary['change_language_flag'][lang]}
+                </Link>
+              </li>
+              {socialItems.map((socialItem) => (
+                <li
+                  title={socialItem.label}
+                  key={socialItem.to}
+                  className='flex'>
+                  <a
+                    href={socialItem.to}
+                    target='_blank'
+                    className='outline-none focus:underline hover:underline'>
+                    {socialItem.icon}
+                  </a>
+                </li>
+              ))}
+              <li className='flex'>
+                <a
+                  title='RSS Feed'
+                  href={`https://juancman.dev/${lang}/feed.xml`}
+                  target='_blank'
+                  className='outline-none focus:underline hover:underline'>
+                  <RSSIcon size='1.5rem' />
+                </a>
+              </li>
+            </ul>
+          </section>
+        </nav>
+
+        <section className='flex sm:hidden h-max items-center'>
+          <SheetTrigger title={dictionary['open_side_menu'][lang]}>
+            <HamburgerIcon size={28} />
+          </SheetTrigger>
+        </section>
+      </header>
+
+      <SheetContent
+        side='right'
+        className='w-max text-center px-0 pt-20 border-0 shadow-boxShadow shadow-md bg-dark1'>
+        <SheetHeader>
+          <nav>
             <section>
               <Link
-                className='outline-none focus:underline hover:underline text-xl text-primary focus:text-primaryLight hover:text-primaryLight'
+                className='outline-none text-xl cursor-default w-full px-10 py-4 font-bold text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)] text-primary focus:text-primaryLight hover:text-primaryLight'
                 href={`/${lang}`}>
                 juancmandev
               </Link>
             </section>
-            <section className='hidden sm:flex items-center gap-8'>
-              <ul className='flex items-center gap-8'>
+            <section className='flex flex-col mt-5'>
+              <ul className='flex flex-col gap-2'>
                 {navItems.map((navItem) => (
-                  <li key={navItem.label} className='w-max h-max'>
-                    <Link
-                      href={`/${lang}${navItem.to}`}
-                      className='outline-none font-bold hover:underline focus:underline text-lg'>
-                      {dictionary[navItem.label][lang]}
-                    </Link>
+                  <li key={navItem.label} className='w-full h-max flex'>
+                    <SheetClose asChild>
+                      <Link
+                        href={`/${lang}${navItem.to}`}
+                        className='outline-none cursor-default w-full px-10 py-3 font-bold text-center text-lg hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
+                        {dictionary[navItem.label][lang]}
+                      </Link>
+                    </SheetClose>
                   </li>
                 ))}
               </ul>
             </section>
-            <section className='hidden sm:block'>
-              <ul className='flex items-center gap-6'>
-                <li className='flex'>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Link
-                        className='px-2 focus:underline hover:underline'
-                        href={`/${changeLang}`}>
-                        <p className='text-2xl'>
-                          {changeLang === 'en' ? '🇺🇸' : '🇲🇽'}
-                        </p>
-                      </Link>
-                    </TooltipTrigger>
-                    <TooltipContent
-                      side='bottom'
-                      className='px-2 py-1 bg-dark2 border-0'>
-                      <p>
-                        {lang === 'en'
-                          ? `Cambiar idioma al ${
-                              changeLang === 'en' ? 'Inglés' : 'Español'
-                            }`
-                          : `Change language to ${
-                              changeLang === 'en' ? 'English' : 'Spanish'
-                            }`}
-                      </p>
-                    </TooltipContent>
-                  </Tooltip>
+            <section className='mt-5'>
+              <ul className='flex flex-col gap-2'>
+                <li className='flex h-max'>
+                  <SheetClose asChild>
+                    <Link
+                      title={dictionary['change_language'][lang]}
+                      className='text-xl outline-none cursor-default w-full py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'
+                      href={`/${changeLang}`}>
+                      {dictionary['change_language_flag'][lang]}
+                    </Link>
+                  </SheetClose>
                 </li>
                 {socialItems.map((socialItem) => (
-                  <li key={socialItem.to} className='flex'>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <a
-                          href={socialItem.to}
-                          target='_blank'
-                          className='outline-none focus:underline hover:underline'>
-                          {socialItem.icon}
-                        </a>
-                      </TooltipTrigger>
-                      <TooltipContent
-                        side='bottom'
-                        className='px-2 py-1 bg-dark2 border-0'>
-                        <p>{socialItem.label}</p>
-                      </TooltipContent>
-                    </Tooltip>
+                  <li title={socialItem.label} key={socialItem.to}>
+                    <SheetClose asChild>
+                      <a
+                        href={socialItem.to}
+                        target='_blank'
+                        className='outline-none cursor-default w-full flex justify-center py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
+                        {socialItem.icon}
+                      </a>
+                    </SheetClose>
                   </li>
                 ))}
                 <li className='flex'>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <a
-                        href={`https://juancman.dev/${lang}/feed.xml`}
-                        target='_blank'
-                        className='outline-none focus:underline hover:underline'>
-                        <RSSIcon size='1.5rem' />
-                      </a>
-                    </TooltipTrigger>
-                    <TooltipContent
-                      side='bottom'
-                      className='px-2 py-1 bg-dark2 border-0'>
-                      <p>RSS Feed</p>
-                    </TooltipContent>
-                  </Tooltip>
+                  <a
+                    title='RSS Feed'
+                    href={`https://juancman.dev/${lang}/feed.xml`}
+                    target='_blank'
+                    className='outline-none cursor-default w-full flex justify-center py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
+                    <RSSIcon size='1.5rem' />
+                  </a>
                 </li>
               </ul>
             </section>
           </nav>
-
-          <section className='flex sm:hidden h-max items-center'>
-            <Tippy
-              trigger='none'
-              placement='left'
-              content={lang === 'en' ? 'Open Side Menu' : 'Abrir Menú Lateral'}>
-              <SheetTrigger>
-                <HamburgerIcon size={28} />
-              </SheetTrigger>
-            </Tippy>
-          </section>
-        </header>
-
-        <SheetContent
-          side='right'
-          className='w-max text-center px-0 pt-20 border-0 shadow-boxShadow shadow-md bg-dark1'>
-          <SheetHeader>
-            <nav>
-              <section>
-                <Link
-                  className='outline-none text-xl cursor-default w-full px-10 py-4 font-bold text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)] text-primary focus:text-primaryLight hover:text-primaryLight'
-                  href={`/${lang}`}>
-                  juancmandev
-                </Link>
-              </section>
-              <section className='flex flex-col mt-5'>
-                <ul className='flex flex-col gap-2'>
-                  {navItems.map((navItem) => (
-                    <li key={navItem.label} className='w-full h-max flex'>
-                      <SheetClose asChild>
-                        <Link
-                          href={`/${lang}${navItem.to}`}
-                          className='outline-none cursor-default w-full px-10 py-3 font-bold text-center text-lg hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
-                          {dictionary[navItem.label][lang]}
-                        </Link>
-                      </SheetClose>
-                    </li>
-                  ))}
-                </ul>
-              </section>
-              <section className='mt-5'>
-                <ul className='flex flex-col gap-2'>
-                  <li className='flex h-max'>
-                    <Tippy
-                      placement='left'
-                      content={
-                        lang === 'en'
-                          ? `Cambiar idioma al ${
-                              changeLang === 'en' ? 'Inglés' : 'Español'
-                            }`
-                          : `Change language to ${
-                              changeLang === 'en' ? 'English' : 'Spanish'
-                            }`
-                      }>
-                      <SheetClose asChild>
-                        <Link
-                          className='outline-none cursor-default w-full py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'
-                          href={`/${changeLang}`}>
-                          <p className='text-2xl'>
-                            {changeLang === 'en' ? '🇺🇸' : '🇲🇽'}
-                          </p>
-                        </Link>
-                      </SheetClose>
-                    </Tippy>
-                  </li>
-                  {socialItems.map((socialItem) => (
-                    <li key={socialItem.to}>
-                      <Tippy placement='left' content={socialItem.label}>
-                        <SheetClose asChild>
-                          <a
-                            href={socialItem.to}
-                            target='_blank'
-                            className='outline-none cursor-default w-full flex justify-center py-3 text-center hover:bg-[rgba(0,_0,_0,_0.2)] focus:bg-[rgba(0,_0,_0,_0.2)]'>
-                            {socialItem.icon}
-                          </a>
-                        </SheetClose>
-                      </Tippy>
-                    </li>
-                  ))}
-                  <li className='flex'>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <a
-                          href={`https://juancman.dev/${lang}/feed.xml`}
-                          target='_blank'
-                          className='outline-none focus:underline hover:underline'>
-                          <RSSIcon size='1.5rem' />
-                        </a>
-                      </TooltipTrigger>
-                      <TooltipContent
-                        side='bottom'
-                        className='px-2 py-1 bg-dark2 border-0'>
-                        <p>RSS Feed</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </li>
-                </ul>
-              </section>
-            </nav>
-          </SheetHeader>
-        </SheetContent>
-      </TooltipProvider>
+        </SheetHeader>
+      </SheetContent>
     </Sheet>
   );
 }
