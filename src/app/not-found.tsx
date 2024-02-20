adc78a5723e7993aaaa6e8a4dd85296b1053e665
Juan Carlos Manzanero Domínguez <juancmandev@protonmail.com>
2024-02-20 13:21:47 -0600

    update not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,20 @@
-'use client';
-
-import Error from 'next/error';
+import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
   return (
     <html lang='en'>
-      <body>
-        <Error statusCode={404} />
+      <body className='text-foreground bg-background'>
+        <main className='w-full h-screen grid place-items-center'>
+          <div className='text-center space-y-4'>
+            <h1 className='text-2xl font-semibold'>Error 404: Not found</h1>
+            <p>
+              Don't worry, you can just <strong>go to home</strong>!
+            </p>
+            <Button asChild className='w-full'>
+              <a href='/'>Go to home</a>
+            </Button>
+          </div>
+        </main>
       </body>
     </html>
   );
