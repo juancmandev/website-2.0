24e83ee4d1c6efd71a91f4b22bd4ed502c5e09d0
Juan Manzanero <59628111+juancmandev@users.noreply.github.com>
2024-03-18 23:25:27 +0000

    Update metadata and fly.io machine (#90)
    * add github to es post
    
    * run rss
    
    * fix styles and typos
    
    * fix iframe overflowing
    
    * run rss
    
    * new post: build a fullstack app
    
    * update github repo
    
    * update next.js 14.1
    
    * update styles
    
    * regenerate package-lock
    
    * fix layout padding
    
    * switch npm to pnpm, change imports and other changes
    
    * Remove spanish content, add dynamic importing, fix styling, add mdx functionalities (#78)
    
    * site now only english
    
    * codeblocks now have copy button with the content inside to copy, fix inline code
    
    * fix codeblocks, add dynamic importing
    
    * remove last date build
    
    * fix nav items
    
    * optimize images
    
    * compress images
    
    * update robots
    
    * update not-found
    
    * update content
    
    * fix '
    
    * update names and format date
    
    * fix date
    
    * new post
    
    * add credits of the photo
    
    * add aspect ratio to item card and content images, update home screen, remove footer bg
    
    * remove underline from web developer
    
    * remove repeated
    
    * remove smooth scroll behavior from back to top button
    
    * remove title from social media
    
    * add about and contact, change images
    
    * revert Image to img
    
    * fix grammar
    
    * add link to written by human, not by ai
    
    * integrate pocketbase
    
    * remove supabase
    
    * uninstall supabase dependencies and remove package-lock
    
    * change microblog bg
    
    * remove next.js Image
    
    * add next toploader component
    
    * change back to top button position
    
    * update contact
    
    * remove microblog item bg
    
    * update fly.io machine
    
    * update metadata
    
    * add prettier config

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,11 @@
 import { getContent } from '@/utils/get-content';
 import Mdx from '@/components/mdx-component';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description: 'Say hi or hire me.',
+};
 
 export default async function Contact() {
   const contact = await getContent('contact', 'content');
